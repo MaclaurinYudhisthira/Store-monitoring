8bd048cc3aa09b2212990d347b4a7573f5e95e54
unknown <kishandasondhi123@gmail.com>
2023-03-27 22:47:08 +0530

    mod

--- src/csv to db.py
+++ src/csv to db.py
@@ -1,19 +1,20 @@
 import pandas as pd
 from sqlalchemy import create_engine
-from models import db, Store, BusinessHours, StoreStatus
+from models import db, Timezone, BusinessHours, Store_status
 from app import app
 
 # Read data from CSV files
-store_df = pd.read_csv('stores.csv')
-business_hours_df = pd.read_csv('business_hours.csv')
-store_status_df = pd.read_csv('store_status.csv')
+timezone_df = pd.read_csv('E:\\Kishan Dasondhi\\Work\\Store-monitoring\\CSV files\\time zones.csv')
+business_hours_df = pd.read_csv('E:\\Kishan Dasondhi\\Work\\Store-monitoring\\CSV files\\Menu hours.csv')
+store_status_df = pd.read_csv('E:\\Kishan Dasondhi\\Work\\Store-monitoring\\CSV files\\store status.csv')
 
 # Connect to SQLite database
-engine = create_engine('sqlite:///database.db')
+engine = create_engine('sqlite:///E:\Kishan Dasondhi\Work\Store-monitoring\src\instance\database.db')
 with app.app_context():
     db.create_all()
 
 # Insert data into SQLite database
-store_df.to_sql('store', con=engine, if_exists='append', index=False)
-business_hours_df.to_sql('business_hours', con=engine, if_exists='append', index=False)
 store_status_df.to_sql('store_status', con=engine, if_exists='append', index=False)
+timezone_df.to_sql('timezone', con=engine, if_exists='append', index=False)
+business_hours_df.to_sql('businesshours', con=engine, if_exists='append', index=False)
+
--- src/models.py
+++ src/models.py
@@ -2,20 +2,32 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Store(db.Model):
+class Store_status(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    timezone = db.Column(db.String(50))
+    store_id = db.Column(db.Integer)
+    timestamp_utc = db.Column(db.DateTime)
+    status = db.Column(db.String(50))
+    def __init__(self,store_id,timestamp_utc,status):
+        self.store_id=store_id
+        self.timestamp_utc=timestamp_utc
+        self.status=status
+
+class Timezone(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    store_id = db.Column(db.Integer, db.ForeignKey('store_status.store_id'))
+    timezone_str = db.Column(db.String(50))
+    def __init__(self,store_id,timezone):
+        self.store_id=store_id
+        self.timezone_str=timezone_str
 
 class BusinessHours(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    store_id = db.Column(db.Integer, db.ForeignKey('store.id'))
+    store_id = db.Column(db.Integer, db.ForeignKey('store_status.store_id'))
     day_of_week = db.Column(db.Integer)
-    start_time = db.Column(db.Time)
-    end_time = db.Column(db.Time)
-
-class StoreStatus(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    store_id = db.Column(db.Integer, db.ForeignKey('store.id'))
-    timestamp_utc = db.Column(db.DateTime)
-    status = db.Column(db.String(50))
+    start_time_local = db.Column(db.Time)
+    end_time_local = db.Column(db.Time)
+    def __init__(self,store_id,day_of_week,start_time_local,end_time_local):
+        self.store_id=store_id
+        self.day_of_week=day_of_week
+        self.start_time=start_time
+        self.end_time=end_time
